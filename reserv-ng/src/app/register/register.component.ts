028af9fd9ad6b615accf6806e4941d58ecf6d99f
unknown <a.bachour.tn@gmail.com>
2017-08-25 08:41:25 +0100

    resgister->login, too - backend basically - sprint1

--- reserv-ng/src/app/register/register.component.ts
+++ reserv-ng/src/app/register/register.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Client } from '../_models/Client';
 import {RegisterService} from "../_services/register.service";
 import { Router, ActivatedRoute } from '@angular/router';
+import {AppComponent} from "../app.component";
+import {LoginService} from "../_services/login.service";
+import {UserCredentials} from "../_models/UserCredentials";
 
 @Component({
   selector: 'app-register',
@@ -11,7 +14,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class RegisterComponent implements OnInit {
   client;
 
-  constructor(private registerService: RegisterService, private router: Router) {
+  constructor(private registerService: RegisterService, private router: Router, private loginService: LoginService, public appComponent: AppComponent) {
     this.client=new Client();
   }
 
@@ -19,6 +22,13 @@ export class RegisterComponent implements OnInit {
   }
   register(){
     this.registerService.register(this.client);
+
+    let userCredentials= new UserCredentials();
+    userCredentials.email=this.client.email;
+    userCredentials.password=this.client.password;
+    var b= this.loginService.login(userCredentials);
+
+    this.appComponent.connected= this.loginService.isLoggedIn();
     this.router.navigate(['']);
   }
 
