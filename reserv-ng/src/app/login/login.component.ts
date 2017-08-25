8e5b715ed3088e2b00b22f300b9e687ed29e14d9
unknown <a.bachour.tn@gmail.com>
2017-08-25 16:36:08 +0100

    tatatata

--- reserv-ng/src/app/login/login.component.ts
+++ reserv-ng/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import {LoginService} from "../_services/login.service";
 import {UserCredentials} from "../_models/UserCredentials";
 import { Router, ActivatedRoute } from '@angular/router';
 import {AppComponent} from "../app.component";
+import {MdDialog} from "@angular/material";
 
 @Component({
   selector: 'app-login',
@@ -13,14 +14,16 @@ export class LoginComponent implements OnInit {
   userCredentials;
   returnUrl: string;
 
-  constructor(private loginService: LoginService, private route: ActivatedRoute, private router: Router, public appComponent: AppComponent) { }
+  constructor(private loginService: LoginService, private route: ActivatedRoute, private router: Router, public appComponent: AppComponent, private dialog: MdDialog) { }
 
   ngOnInit() {
+    this.dialog.closeAll();
     this.userCredentials=new UserCredentials();
     //this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
   login(){
+
     var b= this.loginService.login(this.userCredentials);
     this.appComponent.connected= this.loginService.isLoggedIn();
     this.router.navigate(['']);
