ebb3a41551f14fad22c60d6bd7f4c0b3a0f30187
unknown <a.bachour.tn@gmail.com>
2017-08-30 18:17:57 +0100

    validation reserv 1 but a prob with password

--- reserv-ng/src/app/reserver/reserver.component.ts
+++ reserv-ng/src/app/reserver/reserver.component.ts
@@ -5,6 +5,7 @@ import {MD_DIALOG_DATA} from '@angular/material';
 import { Client } from '../_models/Client';
 import { OffreRes } from '../_models/OffreRes';
 import { DataOffreResAndClientAndState } from '../_models/DataOffreResAndClientAndState';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reserver',
@@ -17,6 +18,7 @@ export class ReserverComponent implements OnInit {
   dataOffreResAndClientAndState: DataOffreResAndClientAndState;
   creerNouveauCompte;
   @ViewChild('tata') el:ElementRef;
+  res1Form: FormGroup;
 
   constructor(private dialog: MdDialog, @Inject(MD_DIALOG_DATA) public offreRes: OffreRes) {
     this.client= new Client();
@@ -25,13 +27,46 @@ export class ReserverComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.createForm();
+  }
 
+  ngOnChanges(){
+    console.log("+change");
+  }
+  createForm(){
+    this.res1Form = new FormGroup({
+      // tslint:disable-next-line
+      nom: new FormControl('', [
+        Validators.minLength(3),
+        Validators.maxLength(50),
+        Validators.pattern("[a-zA-Z ]*"),
+        Validators.required
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$")
+      ]),
+      tel: new FormControl('', [
+        Validators.minLength(8),
+        Validators.pattern("[0-9]+"),
+      ]),
+      'checking' : new FormControl("", [])
+    });
   }
   popout2(){
 
     this.dialog.closeAll();
     this.creerNouveauCompte=this.el.nativeElement.checked;
     this.dataOffreResAndClientAndState.creerNouveauCompte= this.creerNouveauCompte;
+
+    if (this.creerNouveauCompte==true){
+      this.res1Form.addControl(
+        'password', new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})"),
+      ]))
+      )
+    }
 
     this.dataOffreResAndClientAndState.client=this.client;
 
